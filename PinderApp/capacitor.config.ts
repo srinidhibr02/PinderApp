72eaef751daee1846727c783d7e7e74389b0e27f
Srinidhi B R <srinidhi.br@robosoftin.com>
2024-01-17 14:54:50 +0530

    Android Build

--- PinderApp/capacitor.config.ts
+++ PinderApp/capacitor.config.ts
@@ -1,9 +1,9 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'com.example.app',
   appName: 'PinderApp',
-  webDir: 'www',
+  webDir: 'www/browser',
   server: {
     androidScheme: 'https'
   }
